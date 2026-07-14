fe87cb97e74281265cbf6105933b9ebf86d4b4ad
amine sakhri <sakhriaminee2003@gmail.com>
2026-07-14 13:53:16 +0100

    ajoute les elements 'tables' de projet Mvc

---
+++ app/Http/Controllers/PortfolioProjectTagController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\portfolio_project_tag;
+use App\Http\Requests\Storeportfolio_project_tagRequest;
+use App\Http\Requests\Updateportfolio_project_tagRequest;
+
+class PortfolioProjectTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeportfolio_project_tagRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(portfolio_project_tag $portfolio_project_tag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(portfolio_project_tag $portfolio_project_tag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateportfolio_project_tagRequest $request, portfolio_project_tag $portfolio_project_tag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(portfolio_project_tag $portfolio_project_tag)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PortfolioProjectsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\portfolio_projects;
+use App\Http\Requests\Storeportfolio_projectsRequest;
+use App\Http\Requests\Updateportfolio_projectsRequest;
+
+class PortfolioProjectsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeportfolio_projectsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(portfolio_projects $portfolio_projects)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(portfolio_projects $portfolio_projects)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateportfolio_projectsRequest $request, portfolio_projects $portfolio_projects)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(portfolio_projects $portfolio_projects)
+    {
+        //
+    }
+}
