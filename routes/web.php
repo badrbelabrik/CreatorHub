ad3c070e468a0b10e574fb85c346f2c091803793
sarafasra <sarafasraoui75@gmail.com>
2026-07-16 10:29:00 +0100

    ajouter les routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WorkspaceController;
 
 Route::get('/', function () {
     return view('welcome');
+    Route::middleware('auth')->group(function () {
+
+    Route::get('/workspaces', [WorkspaceController::class, 'index'])
+        ->name('workspaces.index');
+
+    Route::get('/workspaces/create', [WorkspaceController::class, 'create'])
+        ->name('workspaces.create');
+
+    Route::post('/workspaces', [WorkspaceController::class, 'store'])
+        ->name('workspaces.store');
+
+});
 });
