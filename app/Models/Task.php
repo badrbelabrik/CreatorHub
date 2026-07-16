08445a4eb9fbd9ed4586bc990812485351d6b87d
sarafasra <sarafasraoui75@gmail.com>
2026-07-16 10:00:05 +0100

    ajouter model de table task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'workspace_id',
+        'assigned_to',
+        'title',
+        'description',
+        'status',
+    ];
+
+    public function workspace()
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function delivery()
+    {
+        return $this->hasOne(Delivery::class);
+    }
 }
