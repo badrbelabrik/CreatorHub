24b8ee131b85167dd595b97eb444ae2ab5fef360
sarafasra <sarafasraoui75@gmail.com>
2026-07-16 10:00:54 +0100

    ajouter model de table delivery

--- app/Models/Delivery.php
+++ app/Models/Delivery.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Delivery extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'task_id',
+        'link',
+    ];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
 }
