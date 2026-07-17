08e1e28b83705f7d22461e00792c37ac3941a1eb
badrbelabrik <badr.belabrik@gmail.com>
2026-07-17 18:26:30 +0100

    add all the api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\FeedController;
+use App\Http\Controllers\Api\PortfolioProjectController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DeliveryController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\WorkspaceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\JobOfferController;
 use App\Http\Controllers\Api\ApplicationController;
@@ -9,3 +15,24 @@ Route::apiResource('jobs', JobOfferController::class);
 Route::post('/jobs/{job}/apply', [ApplicationController::class, 'store']);
 
 Route::get('/jobs/{job}/applications', [ApplicationController::class, 'index']);
+
+Route::apiResource('workspaces', WorkspaceController::class);
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
+Route::post('workspaces/{workspace}/invite', [WorkspaceController::class, 'invite']);
+
+Route::apiResource('tasks', TaskController::class);
+
+Route::apiResource('deliveries', DeliveryController::class);
+
+Route::get('/test', [FeedController::class, 'index']);
+
+
+// Route::middleware('auth:sanctum')
+// ->post('/projects',[PortfolioProjectController::class, 'store']);
+
+
+Route::middleware('auth:sanctum')->post(
+    '/projects',
+    [PortfolioProjectController::class, 'store']
+);
