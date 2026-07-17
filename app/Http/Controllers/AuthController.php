f0cc249b75d201ccd43630282f572c910ff43261
sarafasra <sarafasraoui75@gmail.com>
2026-07-17 16:02:47 +0100

    ajouter authentification

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+       
+
+        return response()->json([
+    'message' => 'User registered successfully!',
+    'user' => $user
+], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (! $user || ! Hash::check($validated['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Les information entrer est incorrect'],
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Logged in successfully!',
+            'token_type' => 'Bearer',
+            'user' => $user
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully!'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WorkspaceController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DeliveryController;
 
 Route::apiResource('workspaces', WorkspaceController::class);
- 
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
 Route::post('workspaces/{workspace}/invite', [WorkspaceController::class, 'invite']);
-Route::apiResource('tasks',TaskController::class);
+
+Route::apiResource('tasks', TaskController::class);
+
+Route::apiResource('deliveries', DeliveryController::class);
