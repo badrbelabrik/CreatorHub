47658ecb9acdc5d3ba41fe80515a690365ebc756
sarafasra <sarafasraoui75@gmail.com>
2026-07-17 10:39:38 +0100

    ajouter le route api d'un controller taskcontroller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\WorkspaceController;
 use App\Http\Controllers\TaskController;
 
 Route::apiResource('workspaces', WorkspaceController::class);
-
+ 
 Route::post('workspaces/{workspace}/invite', [WorkspaceController::class, 'invite']);
 Route::apiResource('tasks',TaskController::class);
